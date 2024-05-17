063310ad36799156a3876dce234a500fca44c812
Jason Pollentier <jason.pollentier@revelry.co>
2024-05-17 17:35:08 -0500

    oops, gotta use dashes to be yaml-y

--- generate-artifact.exs
+++ generate-artifact.exs
@@ -2,11 +2,11 @@ Mix.install([
   {:prodops_ex, "~> 0.1.0"}
 ])
 
-prodops_api_key = System.get_env("INPUT_PRODOPS_API_KEY")
-prompt_template_id = "INPUT_PROMPT_TEMPLATE_ID" |> System.get_env() |> String.to_integer()
-project_id = "INPUT_PROJECT_ID" |> System.get_env() |> String.to_integer()
-artifact_type_slug = System.get_env("INPUT_ARTIFACT_TYPE_SLUG")
-input_count = "INPUT_INPUT_COUNT" |> System.get_env() |> String.to_integer()
+prodops_api_key = System.get_env("INPUT_PRODOPS-API-KEY")
+prompt_template_id = "INPUT_PROMPT-TEMPLATE-ID" |> System.get_env() |> String.to_integer()
+project_id = "INPUT_PROJECT-ID" |> System.get_env() |> String.to_integer()
+artifact_type_slug = System.get_env("INPUT_ARTIFACT-TYPE-SLUG")
+input_count = "INPUT_INPUT-COUNT" |> System.get_env() |> String.to_integer()
 
 parse_inputs = fn count ->
   0..count
@@ -14,8 +14,8 @@ parse_inputs = fn count ->
   |> tl()
   |> Enum.map(fn index ->
     %{
-      name: System.get_env("INPUT_INPUT_#{index}_NAME"),
-      value: System.get_env("INPUT_INPUT_#{index}_VALUE")
+      name: System.get_env("INPUT_INPUT-#{index}-NAME"),
+      value: System.get_env("INPUT_INPUT-#{index}-VALUE")
     }
   end)
 end
