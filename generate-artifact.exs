8511dc9dcc3f885bffdb70c86b87774516f08b4d
Jason Pollentier <jason.pollentier@revelry.co>
2024-05-17 18:51:35 -0500

    try adding  outputs

--- generate-artifact.exs
+++ generate-artifact.exs
@@ -2,6 +2,7 @@ Mix.install([
   {:prodops_ex, "~> 0.1.0"}
 ])
 
+# parse the inputs
 prodops_api_key = System.get_env("INPUT_PRODOPS_API_KEY")
 prodops_api_url = System.get_env("INPUT_PRODOPS_API_URL")
 
@@ -25,6 +26,11 @@ parse_inputs = fn count ->
   end)
 end
 
+# configure the ProdOps SDK with our settings
+Application.put_env(:prodops_ex, :api_key, prodops_api_key)
+Application.put_env(:prodops_ex, :api_url, prodops_api_url)
+
+# create the artifact
 params = %{
   prompt_template_id: prompt_template_id,
   artifact_slug: artifact_type_slug,
@@ -32,11 +38,18 @@ params = %{
   project_id: project_id
 }
 
-Application.put_env(:prodops_ex, :api_key, prodops_api_key)
-Application.put_env(:prodops_ex, :api_url, prodops_api_url)
-
-{:ok, %{response: %{"artifact" => %{"id" => artifact_id}, "url" => artifact_url}}} =
+{:ok, %{response: %{"artifact" => %{"content" => content}, "url" => artifact_url}}} =
   ProdopsEx.Artifact.create(params)
 
-IO.inspect(artifact_id, label: "artifact_id")
-IO.inspect(artifact_url, label: "url")
+# write the outputs
+output_file = System.get_env("GITHUB_OUTPUT")
+{:ok, handle} = File.open(output_file, [:append])
+IO.write(handle, "artifact_url=#{artifact_url}")
+
+IO.write(handle, """
+artifact_content<<ARTIFACT_EOF
+#{content}
+ARTIFACT_EOF
+""")
+
+File.close(handle)
